d58da2ea3e2b40663daab8d00fe80e459903cc09
Alexander Buturlinov <alexander.buturlinov@jetbrains.com>
2013-07-08 12:55:08 +0400

    Added information about importing from Bugzill 2

--- python/bugzilla/defaultBzMapping.py
+++ python/bugzilla/defaultBzMapping.py
@@ -39,6 +39,38 @@ bugzilla.FIELD_NAMES = {
     "component"         :   "Subsystem",
 }
 
+# *** Import from Bugzilla v2.XX ***
+#
+# !!! Important notes !!!
+#  1. You have to alter your Bugzilla database to make it possible to import your data.
+#  2. We strongly recommend you to backup the database before you start.
+#
+# Execute these queries to alter your database:
+#   alter table fielddefs add column `custom` tinyint(4) NOT NULL DEFAULT '0';
+#   alter table fielddefs add column `type` smallint(6) NOT NULL DEFAULT '0';
+#   alter table versions add column `id` mediumint(9) NOT NULL DEFAULT '0';
+#
+# Also, please uncomment the bugzilla.FIELD_NAMES block below and comment the same block above.
+#
+#bugzilla.FIELD_NAMES = {
+#    "bug_id"            :   "numberInProject",
+#    "reporter"          :   "reporterName",
+#    "version"           :   "Affected versions",
+#    "voters"            :   "voterName",
+#    "assigned_to"       :   "Assignee",
+#    "bug_severity"      :   "Severity",
+#    "bug_status"        :   "Status",
+#    "creation_ts"       :   "created",
+#    "op_sys"            :   "Platform",
+#    "short_desc"        :   "summary",
+#    "cc"                :   "watcherName",
+#    "delta_ts"          :   "updated",
+#    "qa_contact"        :   "QA contact",
+#    "estimated_time"    :   "Estimate",
+#    "target_milestone"  :   "Milestone",
+#    "component"         :   "Subsystem",
+#}
+
 # mapping between cf types in bz and youtrack
 bugzilla.CF_TYPES = {
     "1"             :   "string",   #FIELD_TYPE_FREETEXT
