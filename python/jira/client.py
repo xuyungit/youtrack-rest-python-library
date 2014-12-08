050e84eaee2f504601a3e8f09c223cd4d8e434d1
Alexander Buturlinov <alexander.buturlinov@jetbrains.com>
2014-12-08 22:01:33 +0300

    minor

--- python/jira/client.py
+++ python/jira/client.py
@@ -6,13 +6,14 @@ from jira import JiraException
 
 _debug = os.environ.get('DEBUG')
 
+
 class JiraClient(object):
 
     def __init__(self, url, login, password):
         self._url = (url[:-1] if (url[-1] == '/') else url) + "/rest"
         self._headers = {}
         self._http = httplib2.Http(timeout=10, disable_ssl_certificate_validation=True)
-#        self._http.add_credentials(login, password)
+        # self._http.add_credentials(login, password)
         self._login(login, password)
 
     def get_issue_link_types(self):
@@ -25,27 +26,23 @@ class JiraClient(object):
 
     def get_issues(self, project_key, from_id, to_id):
         issues = []
-        for i in range(from_id, to_id):
-            issue = self.get_issue(project_key + "-" + str(i))
+        for i in range(from_id, to_id + 1):
+            issue = self.get_issue('%s-%d' % (project_key, i))
             if issue is not None:
                 issues.append(issue)
         return issues
 
     def get_issue(self, issue_id):
         response, content = self._get(self._rest_url() + "/issue/" + issue_id)
-        if response.status == 200 :
+        if response.status == 200:
             return content
-        else:
-            print "Can't get issue " + issue_id
-            return None
+        print "Can't get issue " + issue_id
 
     def get_worklog(self, issue_id):
         response, content = self._get(self._rest_url() + "/issue/" + issue_id + '/worklog')
-        if response.status == 200 :
+        if response.status == 200:
             return content
-        else:
-            print "Can't get worklog for issue " + issue_id
-            return None
+        print "Can't get worklog for issue " + issue_id
 
     def _rest_url(self):
         return self._url + "/api/latest"
@@ -63,14 +60,14 @@ class JiraClient(object):
     def _get(self, url):
         if _debug:
             print "DEBUG: _get %s" % url
-        response, content = self._http.request(url, headers = self._headers.copy())
+        response, content = self._http.request(url, headers=self._headers.copy())
         if _debug:
             print "DEBUG: response: %d" % response.status
             print "DEBUG: content: %s" % content
         return response, json.loads(content)
 
     def _login(self, login, password):
-#        response, content = self._post(self._url + "/auth/1/session", {"username": login, "password": password})
-#        self._headers['JSESSIONID'] = content['session']['value']
+        # response, content = self._post(self._url + "/auth/1/session", {"username": login, "password": password})
+        # self._headers['JSESSIONID'] = content['session']['value']
         auth = base64.encodestring(login + ':' + password)
         self._headers['Authorization'] = 'Basic ' + auth
