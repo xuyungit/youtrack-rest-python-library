bfac7dda944aa76ddfc87c59d2cd248d3b9c7da2
Alexander Buturlinov <alexander.buturlinov@jetbrains.com>
2015-07-28 20:49:37 +0300

    fix

--- python/youtrack/connection.py
+++ python/youtrack/connection.py
@@ -28,7 +28,7 @@ def relogin_on_401(f):
         try:
             return f(self, *args, **kwargs)
         except youtrack.YouTrackException, e:
-            if e.response.status not in (401, 403):
+            if e.response.status not in (401, 403, 500):
                 raise e
             self._login(*self._credentials)
             return f(self, *args, **kwargs)
