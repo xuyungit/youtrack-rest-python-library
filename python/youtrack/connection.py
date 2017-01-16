09049362375578099d29b1c94a97d79c5615ed9b
Michael Rush <michaelrush@gmail.com>
2017-01-16 13:55:49 -0800

    spelling correction: attachmnets -> attachments
    (cherry picked from commit f7c2994)

--- python/youtrack/connection.py
+++ python/youtrack/connection.py
@@ -227,7 +227,7 @@ class Connection(object):
             raise e
             
 
-    def _process_attachmnets(self, authorLogin, content, contentLength, contentType, created, group, issueId, name,
+    def _process_attachments(self, authorLogin, content, contentLength, contentType, created, group, issueId, name,
                              url_prefix='/issue/'):
         if contentType is not None:
             content.contentType = contentType
@@ -272,13 +272,13 @@ class Connection(object):
 
     def createAttachment(self, issueId, name, content, authorLogin='', contentType=None, contentLength=None,
                          created=None, group=''):
-        return self._process_attachmnets(authorLogin, content, contentLength, contentType, created, group, issueId,
-            name)
+        return self._process_attachments(authorLogin, content, contentLength, contentType, created, group, issueId,
+                                         name)
 
     def importAttachment(self, issue_id, name, content, authorLogin, contentType, contentLength, created=None,
                          group=''):
-        return self._process_attachmnets(authorLogin, content, contentLength, contentType, created, group, issue_id,
-            name, '/import/')
+        return self._process_attachments(authorLogin, content, contentLength, contentType, created, group, issue_id,
+                                         name, '/import/')
 
 
     def getLinks(self, id, outwardOnly=False):
