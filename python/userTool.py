c6e3c25a10f96c57089c9b462f7a36dc22e3efaa
Alexander Buturlinov <alexander.buturlinov@jetbrains.com>
2017-04-25 12:34:36 +0300

    script to import users only

--- python/userTool.py
+++ python/userTool.py
@@ -1,9 +1,13 @@
+import getopt
+import os
+import sys
 from sync.users import UserImporter
 from youtrack.connection import Connection
 
-def _import_all_users(source, target):
+def _import_all_users(source, target, import_groups):
     print 'Starting user importing'
-    user_importer = UserImporter(source, target, caching_users=False)
+    user_importer = UserImporter(
+        source, target, caching_users=True, import_groups=import_groups)
     start = 0
     imported_number = 0
     users_to_import = source.getUsersTen(start)
@@ -12,7 +16,7 @@ def _import_all_users(source, target):
         imported_number += user_importer.importUsersRecursively(refined_users)
         start += 10
         users_to_import = source.getUsersTen(start)
-        if imported_number % 500 == 0: print 'Imported ' + str(imported_number) + ' users'
+        if imported_number % 20 == 0: print 'Imported ' + str(imported_number) + ' users'
     print 'Finished. Total number of imported users: ' + str(imported_number)
 
 def _delete_all_users_except_root_and_guest(target):
@@ -55,25 +59,59 @@ def _check_login(login, excluded=None):
     if not excluded: excluded = []
     return login not in excluded and '/' not in login
 
-def import_users(source_url, source_login, source_password, target_url, target_login, target_password):
+def import_users(
+        source_url, source_login, source_password,
+        target_url, target_login, target_password, import_groups=False):
 
     source = Connection(source_url, source_login, source_password)
     target = Connection(target_url, target_login, target_password)
 
-    _import_all_users(source, target)
+    _import_all_users(source, target, import_groups)
 #    _delete_all_users_except_root_and_guest(target)
 #    _delete_all_groups_except_initial(target)
 #    _import_one_user_with_groups(source, target, 'batman')
 
 
+def usage():
+    print """
+Usage:
+    %s [OPTIONS] s_url s_user s_pass t_url t_user t_pass
+
+    s_url         Source YouTrack URL
+    s_user        Source YouTrack user
+    s_pass        Source YouTrack user's password
+    t_url         Target YouTrack URL
+    t_user        Target YouTrack user
+    t_pass        Target YouTrack user's password
+
+Options:
+    -h,  Show this help and exit
+    -g,  Import groups for users
+""" % os.path.basename(sys.argv[0])
+
+
 def main():
-    source_url = "http://localhost:8080"
-    source_login = "root"
-    source_password = "root"
-    target_url = "http://localhost:8081"
-    target_login = "root"
-    target_password = "root"
-    import_users(source_url, source_login, source_password, target_url, target_login, target_password)
+    import_groups = False
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hg')
+        for opt, val in opts:
+            if opt == '-h':
+                usage()
+                sys.exit(0)
+            elif opt == '-g':
+                import_groups = True
+        (source_url, source_login, source_password,
+         target_url, target_login, target_password) = args[0:6]
+    except getopt.GetoptError, e:
+        print e
+        usage()
+        sys.exit(1)
+    except ValueError:
+        print 'Not enough arguments'
+        sys.exit(1)
+
+    import_users(source_url, source_login, source_password,
+                 target_url, target_login, target_password, import_groups)
 
 if __name__ == "__main__":
     main()
